68c74b2f3f296968e761fb5383e7cf46d01ad0c7
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-23 22:50:23 +0900

    update: Center, Stack component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css';
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Stack } from '@chakra-ui/react';
 
 function App() {
   return (
     <Box>
-      <Box bgColor="orange.500" w="200px" h="50px" color="white" p="4">
+      <Center bgColor="orange.500" color="white" p="4">
         This is the Box
-      </Box>
-      <Box bgColor="starbucks.500" w="200px" h="50px" color="white" p="4" mt="4">
-        This is the Box
-      </Box>
+      </Center>
+      <Stack direction="row" spacing="12">
+        <Box bgColor="orange.500" w="200px" h="50px" color="white" p="4">
+          This is the Box
+        </Box>
+        <Box bgColor="starbucks.500" w="200px" h="50px" color="white" p="4" mt="4">
+          This is the Box
+        </Box>
+      </Stack>
     </Box>
 );
 }
